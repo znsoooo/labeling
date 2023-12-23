ab96dfabd0efacb58378ab7ea923b8d60bdd5c21
Shixian Li <lsx7@sina.com>
2023-12-23 08:56:38 +0800

    Feat: Show label name on the box

--- labeling.py
+++ labeling.py
@@ -13,107 +13,156 @@ SCREEN_HEIGHT = root.winfo_screenheight() * 0.8
 root.destroy()
 
 
-def label2xyxy(text, wh):
-    x, y, w, h = map(float, text.split()[1:])
-    xyxy = (x - w / 2, y - h / 2, x + w / 2, y + h / 2)
-    return [k * y for k, y in zip(xyxy, wh * 2)]
+def read(path):
+    with open(path, 'a+', encoding='u8') as f:
+        f.seek(0)
+        return f.read()
 
 
-def xyxy2label(xyxy, wh):
-    x1, y1, x2, y2 = xyxy
-    xywh = ((x1 + x2) / 2, (y1 + y2) / 2, x2 - x1, y2 - y1)
-    return '0 ' + ' '.join(str(x / y) for x, y in zip(xywh, wh * 2))
+def write(path, data):
+    if isinstance(data, list):
+        data = '\n'.join(data)
+    with open(path, 'w', encoding='u8') as f:
+        f.write(data)
+    return data
 
 
-test_xyxy, test_wh = [2, 3, 5, 6], [11, 13]
-test_xyxy2 = label2xyxy(xyxy2label(test_xyxy, test_wh), test_wh)
-assert test_xyxy == test_xyxy2, test_xyxy2
+def ReadLabel(path, img_wh):
+    w0, h0 = img_wh
+    boxes = []
+    for line in read(path).splitlines():
+        id, x, y, w, h = map(float, line.split())
+        x1, y1, x2, y2 = (x - w / 2, y - h / 2, x + w / 2, y + h / 2)
+        boxes.append([int(id), x1 * w0, y1 * h0, x2 * w0, y2 * h0])
+    return boxes
+
+
+def SaveLabel(path, img_wh, boxes):
+    w0, h0 = img_wh
+    lines = []
+    for id, x1, y1, x2, y2 in boxes:
+        x, y, w, h = ((x1 + x2) / 2, (y1 + y2) / 2, x2 - x1, y2 - y1)
+        line = f'{id} {x / w0} {y / h0} {w / w0} {h / h0}'
+        lines.append(line)
+    return write(path, lines)
+
+
+def ReadImage(path):
+    img = cv2.imdecode(np.fromfile(path, np.uint8), -1)
+    if len(img.shape) == 2:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    h, w, n = img.shape
+    k = min(SCREEN_WIDTH / w, SCREEN_HEIGHT / h)
+    img = cv2.resize(img, (int(w * k), int(h * k)))
+    return img
+
+
+def Hsv2Bgr(h=1.0, s=1.0, v=1.0):
+    return cv2.cvtColor(np.uint8([[[h * 180, s * 255, v * 255]]]), cv2.COLOR_HSV2BGR)[0][0].tolist()
+
+
+def DrawBox(img, rect, label='', color=(0, 0, 255)):
+    x1, x2 = sorted(map(int, rect[0::2]))
+    y1, y2 = sorted(map(int, rect[1::2]))
+    if x1 != x2 and y1 != y2:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+    if label:
+        fontFace, fontScale, thickness = cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1
+        fw, fh = cv2.getTextSize(label, fontFace, fontScale, thickness)[0]
+        cv2.rectangle(img, (x1 - 1, y1), (x1 + fw + 1, y1 - fh - 4), color, -1)
+        cv2.putText(img, label, (x1, y1 - 4), fontFace, fontScale, (255, 255, 255), thickness)
+    return img
 
 
 class Labeling:
     def __init__(self, folder):
         self.img_folder = os.path.join(folder, 'images')
         self.lab_folder = os.path.join(folder, 'labels')
+
+        assert os.path.isdir(self.img_folder), f'Image folder should be exist: "{self.img_folder}"'
         os.makedirs(self.lab_folder, exist_ok=True)
 
-        self.names = os.listdir(self.img_folder)
+        self.files = os.listdir(self.img_folder)
         self.idx = 0
-        self.SetImage()
+        self.NextImage()
 
         cv2.namedWindow(__title__)
-        cv2.setMouseCallback(__title__, self.MouseLoop)
-        self.KeyLoop()
+        cv2.setMouseCallback(__title__, self.MouseEvent)
+        key = 0
+        while key != -1:
+            key = cv2.waitKeyEx(0)
+            self.KeyEvent(key)
 
     @property
     def img_path(self):
-        return os.path.join(self.img_folder, self.names[self.idx])
+        return os.path.join(self.img_folder, self.files[self.idx])
 
     @property
     def lab_path(self):
-        return os.path.join(self.lab_folder, os.path.splitext(self.names[self.idx])[0] + '.txt')
+        return os.path.join(self.lab_folder, os.path.splitext(self.files[self.idx])[0] + '.txt')
 
-    def MouseLoop(self, evt, x, y, flag, param):
-        # print((evt, x, y, flag))
-        if evt == 0 and flag == 1:
+    @property
+    def img_wh(self):
+        return self.img.shape[1::-1]
+
+    def MouseEvent(self, key, x, y, flag, param):
+        # print(f'MouseEvent: {(evt, x, y, flag)}')
+        if key == 0 and flag == 1:
             self.OnLeftDrag(x, y)
-        elif evt == 1:
+        elif key == 1:
             self.OnLeftDown(x, y)
-        elif evt == 4:
+        elif key == 4:
             self.OnLeftUp(x, y)
-        elif evt == 2:
+        elif key == 2:
             self.OnRightDown()
-        elif evt == 10:
-            self.SetImage(1 if flag < 0 else -1)
+        elif key == 10:
+            self.NextImage(1 if flag < 0 else -1)
 
-    def KeyLoop(self):
-        while True:
-            key = cv2.waitKey(0)
-            if key == 13:
-                os.popen('explorer /select, "%s"' % os.path.abspath(self.img_path))
-            elif key == 27:
-                return cv2.destroyAllWindows()
+    def KeyEvent(self, key):
+        # print(f'KeyEvent: {key} ({hex(key)})')
+        if key == 13:
+            os.popen('explorer /select, "%s"' % os.path.abspath(self.img_path))
+        elif key == 27:
+            cv2.destroyAllWindows()
+        elif key in [0x210000, 0x250000, 0x260000]:
+            self.NextImage(-1)
+        elif key in [0x220000, 0x270000, 0x280000]:
+            self.NextImage(1)
 
-    def DrawRects(self):
+    def DrawBoxes(self):
         img = self.img.copy()
-        for i, (x1, y1, x2, y2) in enumerate(self.rects):
-            color = (0, 255, 0) if i == len(self.rects) - 1 else (255, 0, 0)
-            cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+        for i, (id, *rect) in enumerate(self.boxes):
+            DrawBox(img, rect, str(id), Hsv2Bgr(id / 10))
         cv2.imshow(__title__, img)
 
-    def SetImage(self, next=0):
-        self.idx = (self.idx + next) % len(self.names)
-        img = cv2.imdecode(np.fromfile(self.img_path, np.uint8), -1) # read from unicode path
-        h, w, n = img.shape
-        self.k = k = min(SCREEN_WIDTH / w, SCREEN_HEIGHT / h)
-        self.img = cv2.resize(img, (int(w * k), int(h * k)))
-        self.ReadLabels()
+    def NextImage(self, next=0):
+        self.idx = (self.idx + next) % len(self.files)
+        self.img = ReadImage(self.img_path)
+        self.ReadLabel()
 
     def OnLeftDown(self, x, y):
-        self.rects.append([x, y, x, y])
-        self.DrawRects()
+        self.boxes.append([0, x, y, x, y])
+        self.DrawBoxes()
 
     def OnLeftDrag(self, x, y):
-        self.rects[-1][2:] = [x, y]
-        self.DrawRects()
+        self.boxes[-1][-2:] = [x, y]
+        self.DrawBoxes()
 
     def OnLeftUp(self, x, y):
-        self.rects[-1][2:] = [x, y]
-        self.SaveLabels()
+        self.boxes[-1][-2:] = [x, y]
+        self.SaveLabel()
 
     def OnRightDown(self):
-        self.rects = self.rects[:-1]
-        self.SaveLabels()
+        self.boxes = self.boxes[:-1]
+        self.SaveLabel()
 
-    def ReadLabels(self):
-        with open(self.lab_path, 'a+') as f:
-            f.seek(0)
-            self.rects = [label2xyxy(label, self.img.shape[1::-1]) for label in f.read().splitlines()]
-        self.DrawRects()
+    def ReadLabel(self):
+        self.boxes = ReadLabel(self.lab_path, self.img_wh)
+        self.DrawBoxes()
 
-    def SaveLabels(self):
-        with open(self.lab_path, 'w') as f:
-            f.write('\n'.join(xyxy2label(rect, self.img.shape[1::-1]) for rect in self.rects))
-        self.DrawRects()
+    def SaveLabel(self):
+        SaveLabel(self.lab_path, self.img_wh, self.boxes)
+        self.DrawBoxes()
 
 
 if __name__ == '__main__':
